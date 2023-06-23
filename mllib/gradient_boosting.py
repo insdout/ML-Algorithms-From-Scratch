c2b1c5a163701226174adf72a9ba5e8bd4549545
insdout <insdxout@gmail.com>
2023-06-23 21:53:11 +0600

    Updated gradient_boosting.py

--- mllib/gradient_boosting.py
+++ mllib/gradient_boosting.py
@@ -7,6 +7,12 @@ class GradientBoosting(BaseEstimator):
     def __init__(self):
         pass
 
+    def _fit(self, X, y=None):
+        pass
+
+    def predict(self, X):
+        pass
+
 
 if __name__ == "__main__":
     pass
