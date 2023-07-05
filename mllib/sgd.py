02fc3c90c745254045237c0c3af63b9c5b9533e3
insdout <insdxout@gmail.com>
2023-07-05 15:05:53 +0600

    fix style according to PEP8

--- mllib/sgd.py
+++ mllib/sgd.py
@@ -8,8 +8,16 @@ import warnings
 
 
 class SGD(BaseOptimizer):
-     
-    def __init__(self,  gradient_fn, parameters, learning_rate, loss_fn, batch_size=1, verbose=False, **kwargs):
+    def __init__(
+            self,
+            gradient_fn,
+            parameters,
+            learning_rate,
+            loss_fn,
+            batch_size=1,
+            verbose=False,
+            **kwargs
+    ):
         super().__init__(gradient_fn, parameters, learning_rate, **kwargs)
         self.batch_size = batch_size
         self.loss_fn = loss_fn
@@ -23,11 +31,11 @@ class SGD(BaseOptimizer):
         for i in range(0, n_samples, batch_size):
             batch_indices = indices[i:i+batch_size]
             yield X[batch_indices], y[batch_indices]
-    
+
     def update_parameters(self, parameters, gradients):
         parameters -= self.learning_rate * gradients
         return parameters
-    
+
     def optimize(self, X, y):
         iteration = 0
         parameters = self.parameters
@@ -38,13 +46,12 @@ class SGD(BaseOptimizer):
             loss_batch = 0
             for batch in self.batch_generator(X, y):
                 X_batch, y_batch = batch
-            
                 gradients = self.gradient_fn(X_batch, y_batch, parameters)
 
                 parameters = self.update_parameters(parameters, gradients)
                 self.history["parameters"].append(parameters)
                 loss = self.loss_fn(X_batch, y_batch, parameters)
-             
+
                 # Calculate error
                 loss_batch += loss*y_batch.shape[0]
             iteration += 1
@@ -55,19 +62,33 @@ class SGD(BaseOptimizer):
             else:
                 error = abs(prev_loss - loss_batch)
                 if self.verbose:
-                    print(f"{iteration}: prev_loss: {prev_loss :.3f} loss_batch: {loss_batch :.3f} err: {error :.3f}")
+                    print(f"{iteration}: prev_loss: {prev_loss :.3f} "
+                          f"loss_batch: {loss_batch :.3f} err: {error :.3f}")
                 prev_loss = loss_batch
-            
+
         if iteration == self.max_iter:
             warnings.warn("SGD did not converge!")
         return parameters
 
+
 if __name__ == "__main__":
-    loss_fn =  lambda z, y, x: x**2 - 6*x + 9
-    gradient_fn = lambda z, y, x: 2*x - 6
-    sgd = SGD(gradient_fn=gradient_fn, parameters=np.array([20.]), loss_fn=loss_fn, learning_rate=1e-2, max_iter=1000, tolerance=1e-12)
+    def loss_fn(z, y, x):
+        return x**2 - 6*x + 9
+
+    def gradient_fn(z, y, x):
+        return 2*x - 6
+
+    sgd = SGD(
+        gradient_fn=gradient_fn,
+        parameters=np.array([20.]),
+        loss_fn=loss_fn,
+        learning_rate=1e-2,
+        max_iter=1000,
+        tolerance=1e-12
+    )
     X = y = np.array([0])
     x_opt = sgd.optimize(X, y)
     assert abs(x_opt-3) <= 1e2, "Wrong answer"
-    print(f"x_opt: {x_opt} f_opy: {loss_fn(None, None, x_opt)} grad_fn: {gradient_fn(None, None, x_opt)}")
-  
+    print(f"x_opt: {x_opt} f_opy: {loss_fn(None, None, x_opt)} "
+          f"grad_fn: {gradient_fn(None, None, x_opt)}")
+
