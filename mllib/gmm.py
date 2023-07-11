7c56b618bb3eb1143f652e282106f3e74a7d49bf
Mikhail <insdxout@gmail.com>
2023-07-11 20:26:16 +0300

    added template for gmm.py

---
+++ mllib/gmm.py
@@ -0,0 +1,17 @@
+from base import BaseEstimator
+import numpy as np
+
+
+class GMM(BaseEstimator):
+    def __init__(self, n):
+        self.n = n
+
+    def _fit(self):
+        pass
+
+    def _predict(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
