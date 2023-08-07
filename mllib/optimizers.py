fc7a04220c2036aedae98643cd94d37c04b7dca2
insdout <insdxout@gmail.com>
2023-08-07 22:18:52 +0300

    added Adam

--- mllib/optimizers.py
+++ mllib/optimizers.py
@@ -14,7 +14,7 @@ class SGD(BaseOptimizer):
             parameters,
             learning_rate,
             loss_fn,
-            batch_size=1,
+            batch_size=4,
             verbose=False,
             **kwargs
     ):
@@ -63,7 +63,7 @@ class SGD(BaseOptimizer):
                 prev_loss = loss_batch
 
         if iteration == self.max_iter:
-            warnings.warn("SGD did not converge!")
+            warnings.warn("Algorithm did not converge!")
         return parameters
 
 
@@ -89,7 +89,6 @@ class SGDNAG(SGD):
             **kwargs
             ):
         super().__init__(
-            self,
             gradient_fn,
             parameters,
             learning_rate,
@@ -107,90 +106,86 @@ class SGDNAG(SGD):
         return parameters
 
 
-class RMSProp(BaseOptimizer):
+class RMSProp(SGD):
+    """
+    RMSProp algorithm
+    
+    """
     def __init__(
             self,
             gradient_fn,
             parameters,
             learning_rate,
             loss_fn,
-            decay_rate=0.9,
-            epsilon=1e-8,
+            momentum=0.5,
+            alpha=0.9,
+            eps=10e-8,
             batch_size=1,
             verbose=False,
             **kwargs
-    ):
+            ):
         super().__init__(
             gradient_fn,
             parameters,
             learning_rate,
+            loss_fn,
             batch_size,
+            verbose,
             **kwargs
-        )
-        self.loss_fn = loss_fn
-        self.decay_rate = decay_rate
-        self.epsilon = epsilon
-        self.verbose = verbose
-        self.squared_gradients = np.zeros_like(parameters)
+            )
+        self.eps = eps
+        self.momentum = momentum
+        self.alpha = alpha
+        self.velocity = np.zeros_like(parameters)
+        self.delta_gradients = np.zeros_like(parameters)
 
     def update_parameters(self, parameters, gradients):
-        self.squared_gradients = self.decay_rate * self.squared_gradients + (1 - self.decay_rate) * gradients ** 2
-        parameters -= (self.learning_rate / (np.sqrt(self.squared_gradients) + self.epsilon)) * gradients
-        return parameters
-
-    def optimize(self, X, y):
-        iteration = 0
-        parameters = self.parameters
-        error = float("inf")
-        prev_loss = None
-
-        while iteration < self.max_iter and error > self.tol:
-            loss_batch = 0
-            for batch in self.batch_generator(X, y):
-                X_batch, y_batch = batch
-                gradients = self.gradient_fn(X_batch, y_batch, parameters)
-
-                parameters = self.update_parameters(parameters, gradients)
-                self.history["parameters"].append(parameters)
-                loss = self.loss_fn(X_batch, y_batch, parameters)
-
-                # Calculate error
-                loss_batch += loss * y_batch.shape[0]
-
-            iteration += 1
-            loss_batch /= y.shape[0]
-            self.history['loss'].append(loss_batch)
-
-            if prev_loss is None:
-                prev_loss = loss_batch
-            else:
-                error = abs(prev_loss - loss_batch)
-                if self.verbose:
-                    print(f"{iteration}: prev_loss: {prev_loss:.3f} "
-                          f"loss_batch: {loss_batch:.3f} err: {error:.3f}")
-                prev_loss = loss_batch
-
-        if iteration == self.max_iter:
-            warnings.warn("RMSProp did not converge!")
-
+        self.velocity = self.alpha*self.velocity + (1 - self.alpha)*gradients**2
+        self.delta_gradients = self.momentum*self.delta_gradients \
+            - self.learning_rate*gradients/(np.sqrt(self.velocity) + self.eps)
+        parameters += self.delta_gradients
         return parameters
 
 
-class Adam(BaseOptimizer):
-    """_summary_
-
+class Adam(SGD):
+    """
+    ADAM algorithm
     Args:
         BaseOptimizer (_type_): _description_
     """
-    def __init__(self):
-        super().__init__()
-        pass
+    def __init__(
+            self,
+            gradient_fn,
+            parameters,
+            learning_rate,
+            loss_fn,
+            betas=(0.9, 0.99),
+            eps=10e-8,
+            batch_size=1,
+            verbose=False,
+            **kwargs
+            ):
+        super().__init__(
+            gradient_fn,
+            parameters,
+            learning_rate,
+            loss_fn,
+            batch_size,
+            verbose,
+            **kwargs
+            )
+        self.betas = betas
+        self.eps = eps
+        self.momentum = np.zeros_like(parameters)
+        self.velocity = np.zeros_like(parameters)
 
     def update_parameters(self, parameters, gradient):
-        pass
-
-    def optimize(self):
-        pass
+        self.momentum = self.betas[0]*self.momentum + (1-self.betas[0])*gradient
+        self.velocity = self.betas[1]*self.velocity + (1-self.betas[1])* gradient**2
+        momentum_corrected = self.momentum/(1-self.betas[0])
+        velocity_corrected = self.velocity/(1-self.betas[1])
+        parameters -= self.learning_rate/(np.sqrt(velocity_corrected) + self.eps)*momentum_corrected
+        return parameters
 
 
 if __name__ == "__main__":
@@ -214,16 +209,45 @@ if __name__ == "__main__":
     print(f"x_opt: {x_opt} f_opy: {loss_fn(None, None, x_opt)} "
           f"grad_fn: {gradient_fn(None, None, x_opt)}")
   
+    sgdnag = SGDNAG(
+        gradient_fn=gradient_fn,
+        parameters=np.array([20.]),
+        loss_fn=loss_fn,
+        learning_rate=1e-2,
+        max_iter=1000,
+        tolerance=1e-12
+        )
+    X = y = np.array([0])
+    x_opt = sgdnag.optimize(X, y)
+    assert abs(x_opt-3) <= 1e2, "Wrong answer"
+    print(f"x_opt: {x_opt} f_opy: {loss_fn(None, None, x_opt)} "
+          f"grad_fn: {gradient_fn(None, None, x_opt)}")
+
     rmsprop = RMSProp(
         gradient_fn=gradient_fn,
         parameters=np.array([20.]),
         loss_fn=loss_fn,
-        learning_rate=1e-1,
+        learning_rate=1e-2,
         max_iter=1000,
         tolerance=1e-12
         )
     X = y = np.array([0])
     x_opt = rmsprop.optimize(X, y)
+    assert abs(x_opt-3) <= 1e2, "Wrong answer"
+    print(f"x_opt: {x_opt} f_opy: {loss_fn(None, None, x_opt)} "
+          f"grad_fn: {gradient_fn(None, None, x_opt)}")
+    
+    adam = Adam(
+        gradient_fn=gradient_fn,
+        parameters=np.array([20.]),
+        loss_fn=loss_fn,
+        learning_rate=1e-1,
+        max_iter=1000,
+        tolerance=1e-12,
+        batch_size=1
+        )
+    X = y = np.array([0])
+    x_opt = adam.optimize(X, y)
     assert abs(x_opt-3) <= 1e2, "Wrong answer"
     print(f"x_opt: {x_opt} f_opy: {loss_fn(None, None, x_opt)} "
           f"grad_fn: {gradient_fn(None, None, x_opt)}")
